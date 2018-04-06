2b42edc936cd6667a1704ff38b5502212883cf93
coffeelover1404 <nansineelove@hotmail.com>
2018-04-06 21:52:08 +0700

    EggStart ใหม่

---
+++ GotRexTest2/app/src/main/java/com/example/nansi/gotrextest2/EggStart.java
@@ -0,0 +1,35 @@
+package com.example.nansi.gotrextest2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+/**
+ * Skeleton of an Android Things activity.
+ * <p>
+ * Android Things peripheral APIs are accessible through the class
+ * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * set it to HIGH:
+ * <p>
+ * <pre>{@code
+ * PeripheralManagerService service = new PeripheralManagerService();
+ * mLedGpio = service.openGpio("BCM6");
+ * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+ * mLedGpio.setValue(true);
+ * }</pre>
+ * <p>
+ * For more complex peripherals, look for an existing user-space driver, or implement one if none
+ * is available.
+ *
+ * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
+ */
+public class EggStart extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_egg_start);
+
+    }
+}
