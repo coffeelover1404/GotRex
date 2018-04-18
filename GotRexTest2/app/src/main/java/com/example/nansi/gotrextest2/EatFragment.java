de324c3ca16b7ac5fd4557922bc5a7f9542c83f1
meawkeang <meawkeang@gmail.com>
2018-04-19 03:20:51 +0700

    ยังแดร็กไม่ได้ แต่เก็บไว้ก่อย

--- GotRexTest2/app/src/main/java/com/example/nansi/gotrextest2/EatFragment.java
+++ GotRexTest2/app/src/main/java/com/example/nansi/gotrextest2/EatFragment.java
@@ -1,11 +1,17 @@
 package com.example.nansi.gotrextest2;
 
 
+import android.content.ClipData;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 /**
@@ -18,12 +24,80 @@ public class EatFragment extends Fragment {
         // Required empty public constructor
     }
 
-
+    ImageView food1, food2, food3, food4, food5, grex;
+    TextView tt, tgg;
+    View a;
+    ImageView imageView;
+    AnimationDrawable anim;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_eat, container, false);
+        a = inflater.inflate(R.layout.fragment_eat, container, false);
+
+        food1 = (ImageView) a.findViewById(R.id.food1);
+        food2 = (ImageView) a.findViewById(R.id.food2);
+        food3 = (ImageView) a.findViewById(R.id.food3);
+        food4 = (ImageView) a.findViewById(R.id.food4);
+        food5 = (ImageView) a.findViewById(R.id.food5);
+        grex = (ImageView) a.findViewById(R.id.eater);
+        tt = (TextView) a.findViewById(R.id.ttt);
+        tgg = (TextView) a.findViewById(R.id.tgg);
+
+        food1.setOnLongClickListener(longClick);
+        food2.setOnLongClickListener(longClick);
+        food3.setOnLongClickListener(longClick);
+        food4.setOnLongClickListener(longClick);
+        food5.setOnLongClickListener(longClick);
+
+        /*grex.setOnDragListener(dragListener);
+        tgg.setOnDragListener(dragListener);*/
+
+        return a;
     }
+
+    View.OnLongClickListener longClick = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            ClipData data = ClipData.newPlainText("","");
+            View.DragShadowBuilder shadow = new View.DragShadowBuilder(v);
+            v.startDrag(data, shadow, v, 0);
+            return true;
+        }
+    };
+
+
+   View.OnDragListener dragListener = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+
+            int dragEvent = event.getAction();
+
+            switch(dragEvent){
+                case DragEvent.ACTION_DRAG_ENTERED:
+
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    break;
+                case DragEvent.ACTION_DROP:
+
+                    final View view = (View) event.getLocalState();
+
+                    if (view.getId() == R.id.tgg) {
+                        tgg.setText("yey!");
+                        /*imageView = (ImageView) a.findViewById(R.id.eater);
+                        if(imageView == null) throw new AssertionError();
+                        imageView.setBackgroundResource(R.drawable.animation_egg);
+
+                        anim = (AnimationDrawable)imageView.getBackground();
+                        anim.start();*/
+                    }
+
+                    break;
+            }
+
+            return true;
+        }
+    };
 
 }
