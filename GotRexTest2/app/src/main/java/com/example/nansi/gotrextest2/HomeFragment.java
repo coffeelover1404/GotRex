1b998d75912f5b6373b0ee2bc790afbe334c35ce
coffeelover1404 <nansineelove@hotmail.com>
2018-04-20 22:47:30 +0700

    เก็บเวลาได้แล้ว

--- GotRexTest2/app/src/main/java/com/example/nansi/gotrextest2/HomeFragment.java
+++ GotRexTest2/app/src/main/java/com/example/nansi/gotrextest2/HomeFragment.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -14,6 +15,11 @@ import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -25,6 +31,7 @@ public class HomeFragment extends Fragment {
 
     ImageView imageView;
     TextView touchID;
+    TextView check;
     AnimationDrawable anim;
     double value = 0;
 
@@ -91,12 +98,56 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+        try {
+            double newScore = 0;
+            SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+
+            Date systemDate = Calendar.getInstance().getTime(); //Build variable & get current time
+            String myDate = sdf.format(systemDate); //build up string keep current time
+            //check.setText(myDate);
+
+            // TODO: get time from database here
+
+            //parse to date
+            Date Date1 = sdf.parse(myDate);
+            Date Date2 = sdf.parse("02:50:00"); //put what we get from database here
+
+            //calculate to change status
+            long millse = Date1.getTime() - Date2.getTime();
+            long mills = Math.abs(millse);
+            int Hours = (int) (mills/(1000 * 60 * 60));
+            int Mins = (int) (mills/(1000*60)) % 60;
+            long Secs = (int) (mills / 1000) % 60;
+
+            //calculate new score
+            //newScore = mills;
+
+            String diff = Hours + ":" + Mins + ":" + Secs; // updated value every1 second
+            // TODO: put last access back into database
+            //check.setText(diff);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
 
         View myGotRex = inflater.inflate(R.layout.fragment_home, container, false);
         touchID = myGotRex.findViewById(R.id.touchID);
+
+        check = myGotRex.findViewById(R.id.check); //text for checking time
+        /*DateFormat date = new SimpleDateFormat("hh:mm");
+        Date dated = null;
+        String dateFormat = date.format(Calendar.getInstance().getTime());
+        check.setText(dateFormat);*/
+
+        //TextView txtCurrentTime= (TextView)findViewById(R.id.mytext);
+
         imageView = (ImageView)myGotRex.findViewById(R.id.imageBaby);
         if(imageView == null) throw new AssertionError();
         imageView.setBackgroundResource(R.drawable.animation_baby);
