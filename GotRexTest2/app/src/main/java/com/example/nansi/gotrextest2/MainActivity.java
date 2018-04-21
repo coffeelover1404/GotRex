7da1bf38ac943ba76715ec73bff33ef75bdc43b9
coffeelover1404 <nansineelove@hotmail.com>
2018-04-22 01:21:38 +0700

    เก็บเวลาได้แล้ว

--- GotRexTest2/app/src/main/java/com/example/nansi/gotrextest2/MainActivity.java
+++ GotRexTest2/app/src/main/java/com/example/nansi/gotrextest2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.nansi.gotrextest2;
 
 //import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
@@ -8,11 +10,19 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    TextView mTextMessage;
+    TextView text;
+    private GotRexDatabase gotRexDatabase;
     public boolean setName = false;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -26,9 +36,6 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     setTitle("Home");
                     transaction.replace(R.id.content, new HomeFragment()).commit();
-                    //HomeFragment littleGotRex = new HomeFragment();
-                    //fragmentTransaction.replace(R.id.fram, fragment, "HomeFragment");
-                    //fragmentTransaction.commit();
                     return true;
                 case R.id.navigation_eat:
                     setTitle("Eat");
@@ -57,11 +64,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String lastAccess = getLastAccess();
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        mTextMessage = (TextView) findViewById(R.id.mTextMessage);
+        text = (TextView) findViewById(R.id.text);
+        if(lastAccess != "") //have used before
+        {
+            text.setText("Hello have used this app before" + lastAccess);
+            try {
+                double newScore = 0;
+                SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+
+                Date systemDate = Calendar.getInstance().getTime(); //Build variable & get current time
+                String currentTime = sdf.format(systemDate); //build up string keep current time
+
+                //parse to date
+                Date Date1 = sdf.parse(currentTime);
+                Date Date2 = sdf.parse(lastAccess); //put what we get from database here
+
+                //calculate to change status
+                long millse = Date1.getTime() - Date2.getTime();
+                long mills = Math.abs(millse);
+                /*int Hours = (int) (mills/(1000 * 60 * 60));
+                int Mins = (int) (mills/(1000*60)) % 60;
+                long Secs = (int) (mills / 1000) % 60;*/
+                //String diff = Hours + ":" + Mins + ":" + Secs;
+
+                // updated value every1 second
+                double sec = mills/1000;
+                mTextMessage.setText(String.valueOf(sec));
+                gotRexDatabase = new GotRexDatabase(this);
+                gotRexDatabase.open();
+                //gotRexDatabase.updateStatusDifference(sec);
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        //BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
         BottomNavigationViewHelper.removeShiftMode(navigation);
 
         setTitle("Home");
@@ -69,5 +111,33 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.content, new HomeFragment()).commit();
     }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+
+        Date systemDate = Calendar.getInstance().getTime(); //Build variable & get current time
+        String myDate = sdf.format(systemDate); //build up string keep current time
+        saveLastAccess(myDate);
+
+    }
+
+    public void saveLastAccess(String lastTime){
+        SharedPreferences sharedPreferences = getSharedPreferences("lastAccessTime" , MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("lastAccess", lastTime);
+        editor.apply();
+    }
+
+    public String getLastAccess(){
+        SharedPreferences sharedPreferences = getSharedPreferences("lastAccessTime" , MODE_PRIVATE);
+        String lastTime = sharedPreferences.getString("lastAccess", "");
+
+        return lastTime;
+
+    }
+
 
 }
